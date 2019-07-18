a8af2549526f83c9e8cbbc9452d569ca26e9045c
Jailani Abdul Rahman <jailani.har@googlemail.com>
2019-07-18 09:14:14 +0800

    Added Week 2 Practical Class source code

---
+++ week2/Student.java
@@ -0,0 +1,28 @@
+package week2;
+
+public class Student {
+
+	String id;
+	String name;
+	
+	public Student(String sId, String sName) {
+		id = sId;
+		name = sName;
+	}
+	
+	public String attending(boolean attend) {
+		if(attend) {
+			return id + " " + name + " is attending.";
+		} else {
+			return id + " " + name + " is absent.";
+		}
+	}
+}
+
+
+
+
+
+
+
+
